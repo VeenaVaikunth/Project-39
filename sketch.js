741d3229c2c37b10f7c564f76223bb55e7c5bd8b
VeenaVaikunth <95581824+VeenaVaikunth@users.noreply.github.com>
2022-04-17 13:29:16 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var contestantCount;
+var allContestants;
+var answer;
+var database;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+}
+
+function draw(){
+  background(rgb(196, 138, 255));
+  if(contestantCount === 2){
+    quiz.update();
+  }
+  if(gameState === 1){
+    clear();
+    play.update(); 
+  }
+}
